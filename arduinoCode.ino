f56f428b8038dfe24f785a29eca7d51a3384715c
Nile Walker <niwal7@morgan.edu>
2019-12-05 10:54:48 -0500

    Create arduinoCode.ino

---
+++ arduinoCode.ino
@@ -0,0 +1,20 @@
+int RELAY_PIN = ?;
+int SENSOR_PIN1 = ?;
+int SENSOR_PIN2 = ?;
+
+void setup(){
+// put your setup code here, to run once:
+}
+
+// Returns a decimal value representing the current Current Measurement in Amps
+float readData(){}
+
+// Sends Current Measurements to the user interface for visualizing 
+void transmitData(){}
+
+// Enables or disables the relay which switches power to the connectted outlet on/off
+void switchRelay(){}
+
+void loop(){
+// put your main code here, to run repeatedly:
+}
